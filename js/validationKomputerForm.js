8a6b816401e9079b6c9034c216f5536134811f8b
Vladyslav Domariev <s19314@pjwstk.edu.pl>
2020-11-19 23:57:19 +0200

    MP2 Cwiczenie 1/5 Version 0.1

---
+++ js/validationKomputerForm.js
@@ -0,0 +1,56 @@
+
+function validateForm() {
+    const modelInput = document.getElementById('model');
+    const systemOperacyjnyInput = document.getElementById('systemOperacyjny')
+    const typKomputeraInput = document.getElementById('typKomputera');
+    const dataStworzeniaInput = document.getElementById('dataStworzenia');
+
+    const errorModel = document.getElementById('errorModel');
+    const errorSystemOperacyjny = document.getElementById('errorSystemOperacyjny');
+    const errorTypKomputera = document.getElementById('errorTypKomputera');
+    const errorDataStworzenia = document.getElementById('errorDataStworzenia');
+
+    let valid = true;
+
+    if (!checkRequired(modelKomputeraInput.value)) {
+        valid = false;
+        modelInput.classList.add("error-input");
+        errorModel.innerText = "Pole jest wymagane";
+    } else if (!checkTextLengthRange(modelInput.value, 2, 60)) {
+        valid = false;
+        modelInput.classList.add("error-input");
+        errorModelInput.innerText = "Pole powinno zawierać od 2 do 60 znaków";
+    }
+
+    if (!checkRequired(systemOperacyjnyInput.value)) {
+        valid = false;
+        systemOperacyjnyInput.classList.add("error-input");
+        errorSystemOperacyjny.innerText = "Pole jest wymagane";
+    } else if (!checkTextLengthRange(systemOperacyjnyInput.value, 2, 60)) {
+        valid = false;
+        systemOperacyjnyInput.classList.add("error-input");
+        errorSystemOperacyjny.innerText = "Pole powinno zawierać od 2 do 60 znaków";
+    }
+
+    if (!checkRequired(typKomputeraInput.value)) {
+        valid = false;
+        typKomputeraInput.classList.add("error-input");
+        errorTypKomputera.innerText = "Pole jest wymagane";
+    } else if (!checkTextLengthRange(typKomputeraInput.value, 2, 60)) {
+        valid = false;
+        typKomputeraInput.classList.add("error-input");
+        errorTypKomputera.innerText = "Pole powinno zawierać od 2 do 60 znaków";
+    }
+
+    if (!checkRequired(dataStworzeniaInput.value)) {
+        valid = false;
+        dataStworzeniaInput.classList.add("error-input");
+        errorDataStworzenia.innerText = "Pole jest wymagane";
+    }
+
+    if (!valid) {
+        errorSummary.innerText = "Formularz zawiera błędy";
+    }
+
+    return valid;
+}
